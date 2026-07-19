72d17f46655bf49de254e60d7e0b2cf810451fdd
veteztaylor78 <veteztaylor@gmail.com>
2026-07-19 03:54:37 +0000

    Add MAMS Empire POS screens

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MamsEmpireApp());
 }
 
@@ -10,100 +12,16 @@ class MamsEmpireApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'MAMS Empire Food Stuff & Sales',
       debugShowCheckedModeBanner: false,
-      title: 'MAMS Empire POS',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        useMaterial3: true,
+        primaryColor: Colors.green,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+        ),
       ),
       home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-
-  @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (_) => const LoginScreen(),
-        ),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green.shade700,
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            Icon(
-              Icons.store,
-              size: 100,
-              color: Colors.white,
-            ),
-
-            SizedBox(height: 20),
-
-            Text(
-              "MAMS EMPIRE",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            Text(
-              "Food Stuff & Sales",
-              style: TextStyle(
-                color: Colors.white70,
-                fontSize: 18,
-              ),
-            ),
-
-            SizedBox(height: 40),
-
-            CircularProgressIndicator(
-              color: Colors.white,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-      ),
-      body: const Center(
-        child: Text(
-          "Login Screen Coming Next...",
-          style: TextStyle(fontSize: 22),
-        ),
-      ),
     );
   }
 }
